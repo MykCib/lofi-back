23a75743c905cd1e7ed873939f7ccf2ccd723038
MykCib <mykolas.cibas@saldo.com>
2024-07-27 16:19:48 +0300

    initial commit

---
+++ endpoint.py
@@ -0,0 +1,89 @@
+from flask import Flask, jsonify, Response, stream_with_context
+from flask_cors import CORS
+import yt_dlp
+import requests
+import click
+import ffmpeg
+
+app = Flask(__name__)
+CORS(app)  # This enables CORS for all routes
+
+STREAMS = {
+    "1": ("Lofi Girl", "https://www.youtube.com/watch?v=jfKfPfyJRdk"),
+    "2": ("Asian Lofi", "https://www.youtube.com/watch?v=Na0w3Mz46GA"),
+    "3": ("Synthwave Radio", "https://www.youtube.com/watch?v=4xDzrJKXOOY"),
+    "4": ("Rap", "https://www.youtube.com/watch?v=0DztVOeomsk"),
+    "5": ("Chillhop", "https://www.youtube.com/watch?v=5yx6BWlEVcY"),
+}
+
+def get_audio_url(youtube_url):
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'extractaudio': True,
+        'audioformat': 'mp3',
+        'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+        'restrictfilenames': True,
+        'noplaylist': True,
+        'nocheckcertificate': True,
+        'ignoreerrors': False,
+        'logtostderr': False,
+        'quiet': True,
+        'no_warnings': True,
+        'default_search': 'auto',
+        'source_address': '0.0.0.0'
+    }
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(youtube_url, download=False)
+        audio_url = info['url']
+        return audio_url
+
+@app.route('/stream/<stream_id>')
+def get_stream(stream_id):
+    if stream_id not in STREAMS:
+        return jsonify({"error": "Invalid stream ID"}), 400
+    
+    stream_name, youtube_url = STREAMS[stream_id]
+    try:
+        audio_url = get_audio_url(youtube_url)
+        return jsonify({
+            "stream_name": stream_name,
+            "audio_url": f"/proxy_stream/{stream_id}"  # Return a local URL instead
+        })
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+@app.route('/proxy_stream/<stream_id>')
+def proxy_stream(stream_id):
+    if stream_id not in STREAMS:
+        return jsonify({"error": "Invalid stream ID"}), 400
+    
+    youtube_url = STREAMS[stream_id][1]
+    audio_url = get_audio_url(youtube_url)
+    
+    def generate():
+        process = (
+            ffmpeg
+            .input(audio_url)
+            .output('pipe:', format='mp3', acodec='libmp3lame', ac=2, ar='44100', loglevel='quiet')
+            .run_async(pipe_stdout=True)
+        )
+        
+        for chunk in iter(lambda: process.stdout.read(4096), b''):
+            yield chunk
+        
+        process.stdout.close()
+        process.wait()
+
+    return Response(stream_with_context(generate()), mimetype="audio/mpeg")
+
+@app.route('/streams')
+def list_streams():
+    return jsonify({k: v[0] for k, v in STREAMS.items()})
+
+@click.command()
+@click.option('--port', default=3001, help='Port to run the server on')
+def main(port):
+    app.run(host='0.0.0.0', port=port)
+
+if __name__ == "__main__":
+    main()
